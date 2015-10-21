b228bfbbcefb80f7761a4a4de70c5ca00c820159
Saego <usonikru@gmail.com>
2015-10-21 16:17:49 +0300

    All operations

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,15 @@
 public class Calculate{
-    public static void main(String[] arg){
+    public static void main(String[] arg) {
         System.out.println("Hello world...");
+        int first = Integer.valueOf(arg[0]);
+        int second = Integer.valueOf(arg[1]);
+        int summ = first + second;
+        int dif = first - second;
+        int mult = first * second;
+        int div = first / second;
+        System.out.println("Sum  " + summ);
+        System.out.println("Dif  " + dif);
+        System.out.println("Mult  " + mult);
+        System.out.println("Div  " + div);
     }
 }
