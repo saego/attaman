ec8b3ec521bba39c8af9675211967e457495d626
Saego <usonikru@gmail.com>
2015-10-22 23:40:34 +0300

    1

---
+++ Twoclass/Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator{
+
+	//свойства класса
+	//public float first;
+	//public float second;
+	//public double summ;
+	//метод класса, запрос данных для рассчета
+	//public void printCalculator();{ 
+	//System.out.println("Ввести два значения для рассчета("+first+";"+second+")");
+	//}
+	//суммируем первое и второе значения
+	public double summ(float first, float second){
+	double summ = first + second;
+	return summ;
+	}
+
+}
+
---
+++ Twoclass/InteractRunner.java
@@ -0,0 +1,8 @@
+public class InteractRunner{
+
+	public static void main(String[] args){
+		Calculator sum = new Calculator();
+		double result = sum.summ(3, 1);		
+		System.out.println("Summ result:" + result);
+	}
+}
