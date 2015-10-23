0ca960c3a195a73bd73517e2c1b3fdfa015b7577
Saego <usonikru@gmail.com>
2015-10-23 23:52:38 +0300

    New

--- Twoclass/ArgRunner.java
+++ Twoclass/ArgRunner.java
@@ -1,8 +1,13 @@
+	import java.util.Scanner;
 	public class ArgRunner{
-
 	public static void main(String[] arg){
-		float first = Float.valueOf(arg[0]);
-		float second = Float.valueOf(arg[1]);
+		//float first = Float.valueOf(arg[0]);
+		//float second = Float.valueOf(arg[1]);
+		Scanner in = new Scanner (System.in);
+		System.out.print("Fill first data\n");
+		float first = in.nextFloat();
+		System.out.print("Fill second data\n");
+		float second = in.nextFloat();
 		Calculator sum = new Calculator();
 		
 		double result = sum.summ(first, second);		
