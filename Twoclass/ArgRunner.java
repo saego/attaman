c6beded4c9c4b4c574d7956847afef7fc955fce4
Saego <usonikru@gmail.com>
2015-10-27 17:39:44 +0200

    While not finished

--- Twoclass/ArgRunner.java
+++ Twoclass/ArgRunner.java
@@ -1,16 +1,25 @@
 	import java.util.Scanner;
 	public class ArgRunner{
-	public static void main(String[] arg){
+	public static void main(String[] args){
+		try {System.setOut(new java.io.PrintStream(System.out, true, "Cp866")); // Нужно чтобы кириллица корректно выводилась в консоль
+		} catch (Exception ex) {}
 		//float first = Float.valueOf(arg[0]);
-		//float second = Float.valueOf(arg[1]);
+		//float second = Float.valueOf(arg[2]);
+		//String operator = String.valueOf(arg[1]);
 		Scanner inData = new Scanner (System.in);
-		System.out.print("Fill first data\n");
-		float first = inData.nextFloat();
-		System.out.print("Fill second data\n");
-		float second = inData.nextFloat();
-		Calculator sum = new Calculator();
+		String exit = "no";
+		while (!exit.equals("yes")){
+			System.out.print("Fill first data\n");
+			double first = inData.nextDouble();
+			System.out.print("Fill operator\n");
+			String operator = inData.next();
+			System.out.print("Fill second data\n");
+			double second = inData.nextDouble();
 		
-		double result = sum.summ(first, second);		
-		System.out.println("Summ result:" + result);
+        new Calculator(first, operator, second);
+        System.out.print("Exit yes/no: \n");
+			String exit = inData.next();
+        }
+		
 	}
-}
+	}
--- Twoclass/Calculator.java
+++ Twoclass/Calculator.java
@@ -1,18 +1,58 @@
 public class Calculator{
 
-	//свойства класса
-	//public float first;
-	//public float second;
-	//public double summ;
-	//метод класса, запрос данных для рассчета
-	//public void printCalculator();{ 
-	//System.out.println("Ввести два значения для рассчета("+first+";"+second+")");
-	//}
-	//суммируем первое и второе значения
-	public double summ(float first, float second){
-	double summ = first + second;
-	return summ;
-	}
+	private double result;    
+    public void add(double first, double second) {
+        this.result = first + second;
+    }
 
+    public void sub(double first, double second) {
+        this.result = first - second;
+    }
+
+    public void mul(double first, double second) {
+        this.result = first * second;
+    }
+
+    public void div(double first, double second) {
+    this.result = first / second;
+    }
+    public void exp(double first, double second) {
+    this.result = Math.pow(first, second);    
+    }
+    public double getResult(double first, double second){
+    result = this.result;
+    return result;
+    }
+	
+    public Calculator(double first, String operator, double second) {
+        if (operator.equals("+")) {
+        	add(first, second);
+            System.out.println(first + " + " + second + " = " + getResult(first,second));
+            //return;
+        }
+        else if (operator.equals("-")) {
+            sub(first, second);
+        	System.out.println(first + " - " + second + " = " + getResult(first,second));
+            //return;
+        }
+        else if (operator.equals("^")) {
+            exp(first, second);
+        	System.out.println(first + " ^ " + second + " = " + getResult(first,second));
+            //return;
+        }
+        else if (operator.equals("*")) {
+            mul(first, second);
+        	System.out.println(first + " * " + second + " = " + getResult(first,second));
+            //return;
+        }
+        else if (operator.equals("/")) {
+            div(first, second);
+        	System.out.println(first + " / " + second + " = " + getResult(first,second));
+            //return;
+        }
+        else {
+        	System.out.println("Wrong operation");
+    }
+        }
+    
 }
-
