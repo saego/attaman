212fdb6c35b5051ac6171ecdfc3cacb23698ec71
Saego <usonikru@gmail.com>
2015-10-30 15:41:22 +0200

    Final Scan

--- Twoclass/Calculator.java
+++ Twoclass/Calculator.java
@@ -1,61 +1,30 @@
 public class Calculator{
 	
-	private double result;    
-    public void add(int first, int second) {
-        this.result = first + second;
-    }
-
-    public void sub(int first, int second) {
-        this.result = first - second;
-    }
-
-    public void mul(int first, int second) {
-        this.result = first * second;
-    }
-
-    public void div(int first, int second) {
-    	//if (second != 0){
-    	try {	
-    	this.result = first / second;
-    	}
-    	catch(ArithmeticException e){
-    	System.out.println("Division by ZERO!");
-    	}	
-    	//else{throw new RuntimeException("Division by zero");	
-    	//}		
-    }
-    public void exp(int first, int second) {
-    this.result = Math.pow(first, second);    
-    }
-    public double getResult(int first, int second){
-    result = this.result;
-    return result;
-    }
-	
-    public Calculator(int first, String operator, int second) {
+	public double result;    
+	 public void method(double first, String operator, double second) {
         if (operator.equals("+")) {
         	add(first, second);
-            System.out.println(first + " + " + second + " = " + getResult(first,second));
+            //System.out.println(first + " + " + second + " = " + getResult(first,second));
             //return;
         }
         else if (operator.equals("-")) {
             sub(first, second);
-        	System.out.println(first + " - " + second + " = " + getResult(first,second));
+        	//System.out.println(first + " - " + second + " = " + getResult(first,second));
             //return;
         }
         else if (operator.equals("^")) {
             exp(first, second);
-        	System.out.println(first + " ^ " + second + " = " + getResult(first,second));
+        	//System.out.println(first + " ^ " + second + " = " + getResult(first,second));
             //return;
         }
         else if (operator.equals("*")) {
             mul(first, second);
-        	System.out.println(first + " * " + second + " = " + getResult(first,second));
+        	//System.out.println(first + " * " + second + " = " + getResult(first,second));
             //return;
         }
         else if (operator.equals("/")) {
             div(first, second);
-        	System.out.println(first + " / " + second + " = " + getResult(first,second));
+        	//System.out.println(first + " / " + second + " = " + getResult(first,second));
             //return;
         }
         else {
@@ -63,6 +32,41 @@ public class Calculator{
     }
 
     }
+    public void add(double first, double second) {
+        this.result = first + second;
+    }
+
+    public void sub(double first, double second) {
+        this.result = first - second;
+    }
+
+    public void mul(double first, double second) {
+        this.result = first * second;
+    }
+
+    public void div(double first, double second) {
+    	//if (second != 0){
+    	//try {	
+    	this.result = first / second;
+    	//}
+    	//catch(ArithmeticException e){
+    	//System.out.println("Division by ZERO!");
+    	//}	
+    	//else{throw new RuntimeException("Division by zero");	
+    	//}		
+    }
+    public void exp(double first, double second) {
+    this.result = Math.pow(first, second);    
+    }
+    public void clear(){
+    this.result = 0;
+    }
+    public double getResult(){
+    //result = this.result;
+    return result;
+    }
+	
+   
     }
     
 
